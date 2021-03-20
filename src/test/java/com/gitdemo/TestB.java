f779b395e14c84eb8ee9648995b950122f7e5e9f
Marwan Subhi <marwan.subhi4@gmail.com>
2021-03-20 14:50:50 -0400

    added some notes from local

--- src/test/java/com/gitdemo/TestB.java
+++ src/test/java/com/gitdemo/TestB.java
@@ -13,7 +13,9 @@ public class TestB {
 * run the "git add filename" command to add the file to the staging area
 * run the "git commit -m "message"" command to commit the changes
 * run the "git push" command and provide username and password of git hub account
-
+* make some changes from the remote repo , commit and add some message
+* run the "git fetch" command
+* run the "git pull command" and you should be able to see the changes
 *Thank you for your time
 *this is cool
 * */
