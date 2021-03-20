192946d676a5376a56456771994e0d1491ad9171
Marwan Subhi <marwan.subhi4@gmail.com>
2021-03-20 14:44:29 -0400

    added some notes

--- src/test/java/com/gitdemo/TestB.java
+++ src/test/java/com/gitdemo/TestB.java
@@ -7,3 +7,12 @@ public class TestB {
 
     }
 }
+/*
+* First when we write into th terminal we have to run the "git status" command
+* we will see some file that are in red and need to be added
+* run the "git add filename" command to add the file to the staging area
+* run the "git commit -m "message"" command to commit the changes
+* run the "git push" command and provide username and password of git hub account
+*
+*
+* */
